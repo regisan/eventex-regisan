e23afbde8d85cf0f5e510c2b9bb09266cfa2fd2f
Regis Santos <regisan@gmail.com>
2012-03-21 23:28:10 -0300

    Sprint 2: new unittests and success page

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -6,13 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from django.test.client import Client
 
 class SubscriptionFormTest(TestCase):
     def test_new_subscription(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        response = self.client.get('/inscricao')
-        self.assertEquals(301, response.status_code)
-        #self.assertTemplateUsed(response, 'new.html')
+        c = Client()
+        response = c.get('/inscricao/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'base.html')
+        
+    def test_add_subscription(self):
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11111111111', 'email': 'teste@mail.com', 'phone': '11-5555-2222'})
+        self.assertRedirects(response, '/inscricao/1/sucesso/')
+    
+    def test_invalid_subscription(self):
+        response = self.client.post('/inscricao/')
+        self.assertIsNotNone(response.context['messages'])
+    
