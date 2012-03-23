2136ff178e707af15bb15848d3cdcf27b224dae2
Regis Santos <regisan@gmail.com>
2012-03-22 22:57:26 -0300

    Sprint 2 Final

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 from django.conf.urls.defaults import patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -16,8 +16,7 @@ urlpatterns = patterns('django.views.generic.simple',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    #(r'^$', 'core.views.homepage', {'template': 'index.html'}),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^$', 'direct_to_template', {'template': 'index.html'}),
     (r'^inscricao/', include('subscriptions.urls', namespace='subscriptions')),
 )
