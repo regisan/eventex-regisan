4e94ff9729f937b472e836a71024e4c94f9c6d66
Regis Santos <regisan@gmail.com>
2012-03-31 18:58:37 -0300

    Sprint 3: CPF digit validator

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -23,19 +23,19 @@ class SubscriptionFormTest(TestCase):
         self.assertFormError(response, 'form', None, _(u'Você precisa informar seu e-mail ou seu telefone.'))
         
     def test_email_and_phone_blank(self):
-        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11111111111'})
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11144477735'})
         self.assertFormError(response, 'form', None, _(u'Você precisa informar seu e-mail ou seu telefone.'))
     
     def test_add_complete_form(self):
-        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11111111111', 'email': 'teste@mail.com', 'phone_0': '11', 'phone_1': '55552222'})
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11144477735', 'email': 'teste@mail.com', 'phone_0': '11', 'phone_1': '55552222'})
         self.assertRedirects(response, '/inscricao/1/sucesso/')
         
     def test_add_no_email_form(self):
-        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11111111111', 'phone_0': '11', 'phone_1': '55552222'})
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11144477735', 'phone_0': '11', 'phone_1': '55552222'})
         self.assertRedirects(response, '/inscricao/1/sucesso/')
         
     def test_add_no_phone_form(self):
-        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11111111111', 'email': 'teste@mail.com'})
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11144477735', 'email': 'teste@mail.com'})
         self.assertRedirects(response, '/inscricao/1/sucesso/')
     
     def test_cpf_less_than_eleven(self):
@@ -59,27 +59,27 @@ class SubscriptionFormTest(TestCase):
         self.assertFormError(response, 'form', 'phone', 'Ensure this value has at most 20 characters (it has 22).')
         
     def test_duplicated_cpf(self):
-        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11111111111', 'email': 'teste1@mail.com'})
+        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11144477735', 'email': 'teste1@mail.com'})
         self.assertRedirects(response1, '/inscricao/1/sucesso/')
-        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '11111111111', 'email': 'teste2@mail.com'})
+        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '11144477735', 'email': 'teste2@mail.com'})
         self.assertFormError(response2, 'form', 'cpf', _(u'CPF já inscrito.'))
         
     def test_duplicated_email(self):
-        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11111111111', 'email': 'teste@mail.com'})
+        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11144477735', 'email': 'teste@mail.com'})
         self.assertRedirects(response1, '/inscricao/1/sucesso/')
-        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22222222222', 'email': 'teste@mail.com'})
+        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22255588846', 'email': 'teste@mail.com'})
         self.assertFormError(response2, 'form', 'email', _(u'E-mail já inscrito.'))
         
     def test_blank_email_but_not_phone(self):
-        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11111111111', 'phone_0': '11', 'phone_1': '55552222'})
+        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11144477735', 'phone_0': '11', 'phone_1': '55552222'})
         self.assertRedirects(response1, '/inscricao/1/sucesso/')
-        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22222222222', 'phone_0': '11', 'phone_1': '55552222'})
+        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22255588846', 'phone_0': '11', 'phone_1': '55552222'})
         self.assertRedirects(response2, '/inscricao/2/sucesso/')
         
     def test_blank_phone_but_not_email(self):
-        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11111111111', 'email': 'teste@teste.com.br'})
+        response1 = self.client.post('/inscricao/', {'name': 'teste1', 'cpf': '11144477735', 'email': 'teste@teste.com.br'})
         self.assertRedirects(response1, '/inscricao/1/sucesso/')
-        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22222222222', 'email': 'teste2@teste.com.br'})
+        response2 = self.client.post('/inscricao/', {'name': 'teste2', 'cpf': '22255588846', 'email': 'teste2@teste.com.br'})
         self.assertRedirects(response2, '/inscricao/2/sucesso/')
 
     def test_invalid_phone_number(self):
@@ -89,3 +89,8 @@ class SubscriptionFormTest(TestCase):
     def test_invalid_ddd_number(self):
         response = self.client.post('/inscricao/', {'phone_0': '', 'phone_1': '55552222'})
         self.assertFormError(response, 'form', 'phone', u'DDD inválido.')
+        
+    def test_invalid_cpf_digit(self):
+        response = self.client.post('/inscricao/', {'name': 'teste', 'cpf': '11144477730', 'email': 'teste@mail.com'})
+        self.assertFormError(response, 'form', 'cpf', u'CPF inválido.')
+        
--- src/subscriptions/validators.py
+++ src/subscriptions/validators.py
@@ -8,3 +8,52 @@ def CpfValidator(value):
         raise ValidationError(_(u'O CPF deve conter apenas números.'))
     if len(value) != 11:
         raise ValidationError(_(u'O CPF deve ter 11 dígitos.'))
+    if not digito_valido(value):
+        raise ValidationError(_(u'CPF inválido.'))
+        
+def digito_valido(cpf):
+    num = cpf[:-2]
+    digito = cpf[-2:]
+    pesos = range(2,11)
+    pesos.reverse()
+    
+    list_numbers = []
+
+    for n in num:
+        list_numbers.append(int(n))
+        
+    i = 10
+    soma = 0
+    for lst in list_numbers:
+        soma += lst * i
+        i -= 1
+        
+    quoc = soma / 11
+    mod = soma % 11
+    
+    dig1 = 0
+    if (mod >= 2):
+        dig1 = 11 - mod
+    
+    list_numbers.append(dig1)
+    pesos = range(2,12)
+    pesos.reverse()
+    
+    i = 11
+    soma = 0
+    for lst in list_numbers:
+        soma += lst * i
+        i -= 1
+        
+    quoc = soma / 11
+    mod = soma % 11
+    
+    dig2 = 0
+    if (mod >= 2):
+        dig2 = 11 - mod
+    
+    if (str(dig1) + str(dig2)) == digito:
+        return True
+    
+    return False
+ 
