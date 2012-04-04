e478e73a0cc313fab398f390e17f357b5ac9eba7
Regis Santos <regisan@gmail.com>
2012-04-04 00:18:58 -0300

    Sprint4: Custom Managers

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    url = models.URLField(verify_exists=False)
+    description = models.TextField(blank=True)
+    avatar = models.FileField(upload_to='palestrantes', blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class KindContactManager(models.Manager):
+    
+    def __init__(self, kind):
+        super(KindContactManager, self).__init__()
+        self.kind = kind
+        
+    def get_query_set(self):
+        qs = super(KindContactManager, self).get_query_set()
+        return qs.filter(kind=self.kind)
+
+class Contact(models.Model):
+    KINDS = (
+        ('P', _('Telefone')),
+        ('E', _('E-mail')),
+        ('F', _('Fax')),
+    )
+    
+    def __unicode__(self):
+        return '%s, %s' % (self.kind, self.value)
+    
+    speaker = models.ForeignKey('Speaker', verbose_name=_('Palestrantes'))
+    kind = models.CharField(max_length=1, choices=KINDS)
+    value = models.CharField(max_length=255)
+    
+    objects = models.Manager()
+    phones = KindContactManager('P')
+    emails = KindContactManager('E')
+    faxes = KindContactManager('F')
--- src/core/tests.py
+++ src/core/tests.py
@@ -6,6 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from models import Speaker, Contact
 
 
 class HomepageUrlTest(TestCase):
@@ -16,3 +17,20 @@ class HomepageUrlTest(TestCase):
         response = self.client.get('/')
         self.assertEquals(200, response.status_code)
         self.assertTemplateUsed(response, 'index.html')
+
+class SpeakerTest(TestCase):
+    def test_create_speaker(self):
+        s = Speaker(name = 'Bob Marley',
+                    slug = 'bob-marley',
+                    url = 'http://bobmarley.com')
+        s.save()
+        ce = Contact(kind='E', value='bobmarley@reggae.jm')
+        cf = Contact(kind='F', value='11-5555-9999')
+        cp = Contact(kind='P', value='11-5555-8888')
+        s.contact_set.add(ce)
+        s.contact_set.add(cf)
+        s.contact_set.add(cp)
+        self.assertQuerysetEqual(s.contact_set.all().order_by(), 
+                                ['<Contact: E, bobmarley@reggae.jm>',
+                                '<Contact: F, 11-5555-9999>',
+                                '<Contact: P, 11-5555-8888>'])
